3ffb501e019740dc12c0a3573775b10cbcfc4a50
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2016-11-23 10:42:50 +0800

    rebase master

--- src/bin/tikv-server.rs
+++ src/bin/tikv-server.rs
@@ -333,9 +333,8 @@ fn get_rocksdb_raftlog_cf_option(config: &toml::Value) -> RocksdbOptions {
     get_rocksdb_cf_option(config, "raftcf", 256 * 1024 * 1024, false)
 }
 
-fn get_rocksdb_key_cf_option(matches: &Matches, config: &toml::Value) -> RocksdbOptions {
-    get_rocksdb_cf_option(matches,
-                          config,
+fn get_rocksdb_key_cf_option(config: &toml::Value) -> RocksdbOptions {
+    get_rocksdb_cf_option(config,
                           "keycf",
                           256 * 1024 * 1024,
                           true /* bloom filter */)
@@ -457,7 +456,8 @@ fn build_raftkv(config: &toml::Value,
     let cfs_opts = vec![get_rocksdb_default_cf_option(config),
                         get_rocksdb_lock_cf_option(),
                         get_rocksdb_write_cf_option(config),
-                        get_rocksdb_raftlog_cf_option(config)];
+                        get_rocksdb_raftlog_cf_option(config),
+                        get_rocksdb_key_cf_option(config)];
     let mut db_path = path.clone();
     db_path.push("db");
     let engine =
